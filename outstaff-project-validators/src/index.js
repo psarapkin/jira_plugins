07586329cda7a988e0791e2d7f0d72e2f96ce7b7
Peter Sarapkin <psarapkin@gmail.com>
2023-01-06 05:09:11 +0300

    The first initial commit

---
+++ outstaff-project-validators/src/index.js
@@ -0,0 +1,44 @@
+import ForgeUI, { Select, Form, Option, AdminPage, render, useAction, Checkbox, CheckboxGroup, useState, ModalDialog, IssueContext, Text, IssueAction } from "@forge/ui";
+import {properties, asApp, asUser, route, requestJira} from '@forge/api';
+import { view } from '@forge/bridge';
+
+export const run = args => {
+  [issue] = args.issue;
+
+
+  return {
+    result: result,
+    errorMessage: `Issue ${issue.key} is not ready for transition from status ${from.id} to ${to.id}`,
+  }
+};
+
+const CreateProjectInContract = () => {
+  const [isOpen, setOpen] = useState(true)
+
+  if (!isOpen) {
+    return null;
+  }
+
+  let SelectFromList = "Select from List";
+  let testCheckbox = "Test checkbox";
+  return (
+      <ModalDialog header="Creating Project in Contract" onClose={() => setOpen(false)}>
+        <Text>Here will be parameters for creating project issue type </Text>
+        <Form>
+            <CheckboxGroup label={"Check something"} name={"checkboxGroup1"}>
+                <Checkbox label={"Test Checkbox"} value={"Test Checkbox"} />
+            </CheckboxGroup>
+            <Select label={"Select from List"} name={SelectFromList}>
+                <Option label={"Some value1"} value={"value1"} defaultSelected={true} />
+                <Option label={"Some value2"} value={"value2"} />
+            </Select>
+        </Form>
+      </ModalDialog>
+  );
+};
+
+export const createProject = render(
+    <IssueAction>
+      <CreateProjectInContract />
+    </IssueAction>
+);
