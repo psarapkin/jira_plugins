cc0207451f3077630825076013d1b6ee121b311c
Peter Sarapkin <psarapkin@gmail.com>
2023-01-21 00:49:41 +0300

    Made realization for CreateDeveloper dialog. Now on submit a Developer is created and parent link and reletes link also created.

--- outstaff-project-validators/src/index.js
+++ outstaff-project-validators/src/index.js
@@ -22,39 +22,6 @@ const CreateProjectInContract = () => {
   const [isOpen, setOpen] = useState(true);
   let newProjectName = "ProjectName";
 
-
-    const [issuesFromDocs] = useState(async () => {
-      var bodyData = {
-          "expand": [
-            "names",
-            "schema",
-            "operations"
-          ],
-          "jql": "project = DOC",
-          "maxResults": 50,
-          "fieldsByKeys": false,
-          "fields": [
-            "summary",
-            "status",
-            "assignee"
-          ],
-          "startAt": 0
-        };
-
-      const response = await api.asUser().requestJira(route`/rest/api/3/search`, {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(bodyData)
-      });
-
-      return (await response.json()).issues.map(issue => <Option label={issue.fields.summary} value={issue.key} />);
-  });
-
-
-
 /*
 * Создаем новый проект, параметры
 * ProjectName - имя проекта
@@ -104,7 +71,7 @@ const CreateProjectInContract = () => {
             "key": (await createdProjectIssueResponse.json()).key
           },
           "type": {
-              "name": "Parent Link"
+              "name": "Parent"
           }
       };
 
@@ -136,20 +103,9 @@ const CreateProjectInContract = () => {
 };
 
 const CreateDeveloperInProject = () => {
+
     const [isOpen, setOpen] = useState(true);
-
-    const developers_company = "developers_company";
-    const developer_fio = "developer_fio";
-    const developer_email = "developer_email";
-    const currency = "currency";
-    const rate_buy = "rate_buy";
-    const rate_sell = "rate_sell";
-    const resource_start = "resource_start";
-    const resource_end = "resource_end";
-    const manager_fio = "manager_fio";
-    const manager_email = "manager_email";
-
-    const companyDocuments = useState(async () => {
+    const [companyDocuments] = useState(async () =>  {
         var bodyData = `
             {
               "expand": [
@@ -176,11 +132,50 @@ const CreateDeveloperInProject = () => {
             },
             body: bodyData
         });
+        let issues = (await response.json()).issues;
 
-        return (await response.json()).map(issue => <Option label={issue.fields.summary} value={issue.key} />);
+        if (issues !== undefined) {
+            return issues.map(issue => <Option label={issue.fields.summary} value={issue.key} />);
+        }
 
+        return null;
     });
 
+    const [currencyValues] = useState(async () => {
+        const fieldsResponse = await api.asUser().requestJira(route`/rest/api/3/field`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        const currencyField = (await fieldsResponse.json()).filter(field => field.name === "Currency").shift();
+
+        const customFieldContexts = await api.asUser().requestJira(route`/rest/api/3/field/${currencyField.id}/context`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        const optionsContextId =  Number((await customFieldContexts.json()).values[0].id);
+
+        const optionsResponse = await api.asUser().requestJira(route`/rest/api/3/field/${currencyField.id}/context/${optionsContextId}/option`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        return (await optionsResponse.json()).values.map(value => <Option label={value.value} value={value.id} />);
+    });
+
+    const developers_company = "developers_company";
+    const developer_fio = "developer_fio";
+    const developer_email = "developer_email";
+    const currency = "currency";
+    const rate_buy = "rate_buy";
+    const rate_sell = "rate_sell";
+    const resource_start = "resource_start";
+    const resource_end = "resource_end";
+    const manager_fio = "manager_fio";
+    const manager_email = "manager_email";
 
     if (!isOpen) {
         return null;
@@ -189,7 +184,107 @@ const CreateDeveloperInProject = () => {
     const onSubmit = async data => {
         const context = useProductContext();
 
+        const issueTypeResponse = await api.asApp().requestJira(route`/rest/api/3/issuetype`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
 
+        const developerIssueType = (await issueTypeResponse.json()).filter(issue => issue.name === "Developer").shift();
+
+        const fieldsResponse = await api.asUser().requestJira(route`/rest/api/3/field`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        const developerFioField = (await fieldsResponse.json()).filter(field => field.name === "Developer FIO").shift();
+        const developerEmailField = (await fieldsResponse.json()).filter(field => field.name === "Developer Email").shift();
+        const currencyField = (await fieldsResponse.json()).filter(field => field.name === "Currency").shift();
+        const rateBuyField = (await fieldsResponse.json()).filter(field => field.name === "Developer Buy Rate").shift();
+        const rateSellField = (await fieldsResponse.json()).filter(field => field.name === "Developer Sell Rate").shift();
+        const resourceStartDateField = (await fieldsResponse.json()).filter(field => field.name === "Resource Period. Date Start").shift();
+        const resourceEndDateField = (await fieldsResponse.json()).filter(field => field.name === "Resource Period. Date End").shift();
+        const managerFioField = (await fieldsResponse.json()).filter(field => field.name === "Manager FIO").shift();
+        const managerEmailField = (await fieldsResponse.json()).filter(field => field.name === "Manager email").shift();
+
+        let createDeveloperIssueBody = `{
+          "fields": {
+              "summary": "${data.developer_fio}",
+              "project": {
+                  "id": "${context.platformContext.projectId}"
+              },
+              "issuetype": {
+                  "id": "${developerIssueType.id}"
+              },
+              "reporter": {
+                  "id": "${context.accountId}"
+              },
+              "${developerFioField.id}": "${data.developer_fio}",
+              "${developerEmailField.id}": "${data.developer_email}",
+              "${currencyField.id}": {
+                "id": "${data.currency}"
+              },
+              "${rateBuyField.id}": ${data.rate_buy},
+              "${rateSellField.id}": ${data.rate_sell},
+              "${resourceStartDateField.id}": "${data.resource_start}",
+              "${resourceEndDateField.id}": "${data.resource_end}",
+              "${managerFioField.id}": "${data.manager_fio}",
+              "${managerEmailField.id}": "${data.manager_email}"
+          }
+      }`;
+
+        const createdDeveloperIssueResponse = await api.asUser().requestJira(route`/rest/api/3/issue`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: createDeveloperIssueBody
+        });
+
+
+        let createIssueLinksBody = {
+            "outwardIssue": {
+                "key": (await createdDeveloperIssueResponse.json()).key
+            },
+            "inwardIssue": {
+                "key": context.platformContext.issueKey
+            },
+            "type": {
+                "name": "Parent"
+            }
+        };
+
+        await api.asUser().requestJira(route`/rest/api/3/issueLink`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(createIssueLinksBody)
+        });
+
+        let createContractLink = {
+            "outwardIssue": {
+                "key": data.developers_company
+            },
+            "inwardIssue": {
+                "key": (await createdDeveloperIssueResponse.json()).key
+            },
+            "type": {
+                "name": "Relates"
+            }
+        };
+
+        await api.asUser().requestJira(route`/rest/api/3/issueLink`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(createContractLink)
+        });
 
         setOpen(false);
     };
@@ -200,19 +295,17 @@ const CreateDeveloperInProject = () => {
               <Select label={"Select contract from documents"} name={developers_company}>
                   {companyDocuments}
               </Select>
-              <TextField name={"Developer FIO"} label={developer_fio} />
-              <TextField name={"Developer Email"} label={developer_email} type={"email"} />
+              <TextField label={"Developer FIO"} name={developer_fio} />
+              <TextField label={"Developer Email"} name={developer_email} type={"email"} />
               <Select label={"Currency"} name={currency}>
-                  <Option label={"RUB"} value={"RUB"} />
-                  <Option label={"USD"} value={"USD"} />
-                  <Option label={"EUR"} value={"EUR"} />
+                  {currencyValues}
               </Select>
-              <TextField name={"Buy rate"} label={rate_buy} type={"number"} />
-              <TextField name={"Sell rate"} label={rate_sell} type={"number"} />
-              <DatePicker name={"Resource start"} label={resource_start} />
-              <DatePicker name={"Resource end"} label={resource_end} />
-              <TextField name={"Manager FIO"} label={manager_fio} />
-              <TextField name={"Manager email"} label={manager_email} type={"email"} />
+              <TextField label={"Buy rate"} name={rate_buy} type={"number"} />
+              <TextField label={"Sell rate"} name={rate_sell} type={"number"} />
+              <DatePicker label={"Resource start"} name={resource_start} />
+              <DatePicker label={"Resource end"} name={resource_end} />
+              <TextField label={"Manager FIO"} name={manager_fio} />
+              <TextField label={"Manager email"} name={manager_email} type={"email"} />
           </Form>
       </ModalDialog>
     );
